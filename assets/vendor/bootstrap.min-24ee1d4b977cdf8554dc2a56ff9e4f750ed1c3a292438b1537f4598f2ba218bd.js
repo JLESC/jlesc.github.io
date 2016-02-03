3c5a407768bdd5dfc436459f3e784834a2e0dbe8
Travis CI Deployment <travis@jlesc.github.io>
2016-02-03 09:45:36 +0000

    new version Wed Feb  3 09:45:36 UTC 2016

--- assets/vendor/bootstrap.min-24ee1d4b977cdf8554dc2a56ff9e4f750ed1c3a292438b1537f4598f2ba218bd.js
+++ assets/vendor/bootstrap.min-24ee1d4b977cdf8554dc2a56ff9e4f750ed1c3a292438b1537f4598f2ba218bd.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap vv4-dev#48e62fd (http://getbootstrap.com)
+ * Bootstrap v4-dev@5647e0c (http://getbootstrap.com)
  * Copyright 2011-2016 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
