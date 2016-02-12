a21839f954480c2738d504a90b9bf7d2a93f3efb
Torbjörn Klatt <t.klatt@fz-juelich.de>
2016-02-12 08:07:29 +0100

    documentation for the LaTeX report generator
    @pancetta: do you understand those comments?
    
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- bin/report_generator.rb
+++ bin/report_generator.rb
@@ -1,4 +1,80 @@
 #!/usr/bin/env ruby
+=begin rdoc
+coding: UTF-8
+:markup: RDoc
+:title: LaTeX Report Generator
+
+= LaTeX Generator
+
+== About
+This script is an exporter for the projects.
+It generates LaTeX files for each project to be used in the annual JLESC report.
+Each project's LaTeX file is accompanied with a BibTeX file with all citations used by the project.
+
+== Usage
+You must run this script from the source directory of the JLESC site, i.e. the parent of the
+directory this script is in.
+Given you have loaded the correct Ruby environment for the JLESC site
+(cf. {Testing Locally on the JLESC Webpage Wiki}[https://github.com/JLESC/jlesc.github.io/wiki/Testing-Locally]).
+Then simply run it from the command line:
+
+    bundle exec ./bin/report_generator.rb
+
+The LaTeX files will be written to +./latex_out+.
+
+== Details
+This script heavily depends on monkey patching a few central functionalities of _Jekyll_.
+In detail, the patches are:
+
+[+Jekyll::Site#render+]
+
+  It is only considering the site's _projects_ collection of +Jekyll::Document+s.
+  Also, the _topics_ database is read and pre-processed a little bit for further use.
+
+[+Jekyll::Site#write+]
+
+  Writes each rendered and _Pandoc_-processed +Jekyll::Document+'s content of the _projects_
+  collection to the LaTeX file.
+  It also writes out the accompanying BibTeX file for each project.
+  Finally, it generates one LaTeX file per _topic_ including all project files of the _topic_.
+
+[+Jekyll::Document+]
+
+  Got a new attribute +bibtex+ storing the +BibTeX::Bibliography+ with all citations used within
+  the document.
+
+[+Jekyll::Document#destination+]
+
+  Does not construct the output path from the site object but only relative to the given parameter
+  (base path).
+
+[+Jekyll::Renderer#run+]
+
+  This is where the rendering and conversion magic happens.
+  Prior to passing the document's Markdown content to the Liquid processor, all used citations are
+  gathered (cf. new method +Jekyll::Renderer#read_used_citations+) to later construct the document
+  specific BibTeX file.
+  Then, the Markdown content is modified via the new method +Jekyll::Renderer#md_cleanup+ to remove
+  or replace any Liquid tags which will produce plain HTML or other obtrusive code after Liquid
+  processing.
+  After the Liquid engine is done, the output is passed to _Pandoc_ for the final conversion from
+  Markdown to LaTeX.
+  In the end, the converted LaTeX content is again cleaned up and tweaked to match the specific
+  requirements of the JLESC annual report (cf. new method +Jekyll::renderer#tex_cleanup+).
+
+== Future Notes
+As this script was written with a specific set of gems and versions and heavily depends on monkey
+patching, the script might break in unexpected (and possible silent) ways in the future.
+The most important dependencies are the following:
+
+  bibtex-ruby (4.2.0)
+  jekyll (3.1.1)
+  jekyll-scholar (5.7.0)
+  liquid (3.0.6)
+  pandoc-ruby (1.0.0)
+
+For the full list refer to the +Gemfile.lock+ file on commit SHA +9afcb40fbda60cf71e65fb4ebd11b8714b35697c+.
+=end
 
 require 'bundler/setup'
 Bundler.require(:default, :report)
@@ -13,6 +89,7 @@ CITATION_MATCHER = /{%\scite\s(?<bibtex_id>\w*)\s--file\s(?<bibfile>[\w\/]*)\.bi
 
 
 def sanitize_for_latex(content)
+  # TODO: don't re-sanitize those which are already escaped!
   content.gsub! '&', '\\\&'
 end
 
@@ -27,6 +104,7 @@ module Jekyll
 
       Jekyll::Hooks.trigger :site, :pre_render, self, payload
 
+      # TODO: move this into a separate block registered as a :pre_render hook for :site
       @topics = {}
       data['topics'].each_pair do |topic_id, topic_hash|
         topic_hash.update({projects: []})
@@ -64,14 +142,6 @@ module Jekyll
         bibfiles << bibfile
       end
 
-      # bibfile = 'bibfiles.tex'
-      # puts "writing LaTeX include file for BibTeX files #{bibfile} ..."
-      # File.open(File.join(dest, bibfile), 'wb') do |f|
-      #   bibfiles.each do |bib|
-      #     f.write("\\addbibresource{project/#{bib}}\n")
-      #   end
-      # end
-
       puts "writing Topics files to #{dest} ..."
       @topics.each_pair do |topic_id, topic_hash|
         file = "#{topic_id.to_s}.tex"
@@ -98,16 +168,6 @@ module Jekyll
       path = File.join(dest, URL.unescape_path(url))
       path << output_ext unless path.end_with? output_ext
       path
-    end
-
-    def write(dest)
-      path = destination(dest)
-      FileUtils.mkdir_p(File.dirname(path))
-      File.open(path, 'wb') do |f|
-        f.write(output)
-      end
-
-      trigger_hooks(:post_write)
     end
   end
 
@@ -158,6 +218,14 @@ module Jekyll
     end
 
     private
+    ##
+    # Scans the content of the current document for occurrences of _jekyll-scholar_'s +cite+ Liquid
+    # tag and gathers the BibTeX key and the BibTeX source file of the key.
+    #
+    # Returns an array of hashes of the form
+    #
+    #   [{:bibfile => '', :bibtex_id => ''}, ...]
+    #
     def gather_citations
       @citations = []
       document.content.scan CITATION_MATCHER do |bibtex_id, bibfile|
@@ -167,6 +235,15 @@ module Jekyll
     end
 
     private
+    ##
+    # Scans the content of the current document for occurrences of _jekyll-scholar_'s +bibliography+
+    # Liquid tag and gathers the BibTeX source file name.
+    # Each file is open, read and parsed by _bibtex-ruby_.
+    #
+    # Returns hash of the form
+    #
+    #   {BIBFILE_BASENAME => BibTeX::Bibliography}
+    #
     def get_bibfiles
       puts '      reading in BibTeX files'
       @bibfiles = {}
@@ -179,11 +256,19 @@ module Jekyll
     end
 
     private
+    ##
+    # Reads and gathers all used citations of the current document and puts them into a new
+    # +BibTeX::Bibliography+ of the +Jekyll::Document+.
+    # Citations, which originates in the JLESC BibTeX file (i.e. +jlesc.bib+) are tagged with the
+    # keyword _own_ to denote publications created within JLESC.
+    #
+    # Returns nothing.
+    #
     def read_used_citations
       puts '    reading used citations'
 
-      @citations ||= gather_citations
-      @bibfiles ||= get_bibfiles
+      @citations = gather_citations
+      @bibfiles = get_bibfiles
 
       if @citations.length < 1
         puts "WARNING: no citations for project #{document.data['slug']}"
@@ -218,30 +303,50 @@ module Jekyll
     end
 
     private
+    ##
+    # Cleans up the Markdown of the current document.
+    #
     def md_cleanup
       puts '    cleaning up obsolete stuff in Markdown'
+      # generation/output of the bibliography (note: the LaTeX package _biblatex_ is used)
+      # a) the JLESC publications
       document.content.gsub! BIBLIOGRAPHY_MATCHER_JLESC, '\printbibliography[heading=none,keyword=own]'
+      # b) external references
       document.content.gsub! BIBLIOGRAPHY_MATCHER_EXTERNAL, '\printbibliography[heading=none,notkeyword=own]'
+
+      # the table listing the person-months efforts needs a heading _Pandoc_ understands
       document.content.gsub! /{:\.person-months-table.*}/, "|   |   |\n|---+---|"
 
+      # the _jekyll-scholar_ +cite+ Liquid tag needs to be replace as it generates HTML code
       document.content.gsub! CITATION_MATCHER, "\\cite{\\k<bibtex_id>-#{document.data['slug'].gsub(/_/, '-')}}"
     end
 
     private
+    ##
+    # Cleans up the LaTeX as generated by _Pandoc_
+    #
     def tex_cleanup
       puts '    cleanup obsolete stuff in LaTeX'
+      # _Pandoc_ prepends each +enumeration+ with some layout settings; remove that
       document.content.gsub! /\\itemsep.*\n/, ''
 
       puts '    increase heading levels'
-      document.content.gsub! /\\section{(.*)}\\label/, '\subsubsection{\1}\label'
+      # for the annual report the level of headings of the project reports need to be increased
+      # a) project titles need to be two levels down
+      document.content.gsub! /\\section{(.*)}\\label/, '\subsubsection{\1}\label'  # TODO: do we need that here?
+      # b) headings within a project need to be named paragraphs
       document.content.gsub! /\\subsection{(.*)}\\label/, '\paragraph{\1}~\\\label'
 
+      # to fix multiple occurrences of labels in the aggregated report, we prepend each label with
+      # the project's slug
       document.content.gsub! /\\label{([\w-]*)}/, "\\label{#{document.data['slug']}-\\1}"
 
+      # wrap the project's report in it's own _biblatex_ reference section
       document.content.prepend("\\begin{refsection}[projects/#{document.data['slug']}]\n\n")
-      document.content.prepend("\\subsubsection{#{document.data['title']}}\\label{#{document.data['title'].downcase.gsub(/\s/, '-')}}\n\n")
-
       document.content += "\n\\end{refsection}\n"
+
+      # add the project's title
+      document.content.prepend("\\subsubsection{#{document.data['title']}}\\label{#{document.data['title'].downcase.gsub(/\s/, '-')}}\n\n")
     end
   end
 
@@ -265,12 +370,15 @@ module Jekyll
       @site.render
       @site.write
 
+      # create the annual report main file ...
       report_dest = File.join(@latex_path, "report_#{Date.today.year}.tex")
+      # unless it already exists
       unless test('e', report_dest)
         FileUtils.cp(File.join(@site.source, '_templates', 'report', "report.tex"),
                      report_dest)
       end
 
+      # copy the JLESC logo to the LaTeX directory
       FileUtils.cp(File.join(@site.source, '_assets', 'images', 'jlesc_logo.jpg'),
                    File.join(@latex_path, 'jlesc_logo.png'))
 
