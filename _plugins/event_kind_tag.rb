69316e1306f85bf6207f56f00ee3265e05e9d6e1
Torbjörn Klatt <t.klatt@fz-juelich.de>
2016-02-15 12:18:16 +0100

    adding event categories: workshop, summer school, mini symposium
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

---
+++ _plugins/event_kind_tag.rb
@@ -0,0 +1,47 @@
+module Jekyll
+  module Tags
+    class RenderEventKindTagError < StandardError
+      def initialize(msg)
+        super(msg)
+      end
+    end
+
+    class RenderEventKindTag < Liquid::Tag
+      def initialize(tag_name, markup, tokens)
+        super
+        @kind_id = markup.strip
+        @kind = nil
+        @kinds = nil
+      end
+
+      def get_kind(context)
+        @kinds = context.registers[:site].data['event_kinds']
+
+        rendered = Liquid::Template.parse(@kind_id).render(context)
+
+        if @kinds.has_key?(rendered)
+          @kind = @kinds[rendered]
+        else
+          raise RenderEventKindError.new \
+            "EventKindID '#{rendered}' not found. Probably a typo? See _data/event_kinds.yml."
+        end
+      end
+
+      def construct_kind(context)
+        get_kind(context)
+
+        unless @kind.has_key?('title')
+          raise RenderEventKindTagError.new "EventKindID '#{@kind_id}' has no 'title' defined."
+        end
+
+        "<span class=\"event-kind\">#{@kind['title']}</span>"
+      end
+
+      def render(context)
+        "#{construct_kind(context)}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('event_kind', Jekyll::Tags::RenderEventKindTag)
