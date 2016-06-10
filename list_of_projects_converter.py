dc0d957dfe37d1a366ea50ddd545cb6bd349f4eb
Isabel Heisters <i.heisters@fz-juelich.de>
2016-06-10 11:31:28 +0200

    Changed the problems
    Signed-off-by: Isabel Heisters <i.heisters@fz-juelich.de>

--- list_of_projects_converter.py
+++ list_of_projects_converter.py
@@ -2,30 +2,36 @@ import os
 
 file = open('list_of_projects.txt', 'w')
 
-filecut = os.listdir('./_projects')
-filecut.remove('.placeholder')
+filecut = []
+
+for files in os.listdir('./_projects'):
+    if files.endswith(".md"):
+        filecut.append(files)
+
 
 for sFile in filecut:
     content = open('./_projects/'+ sFile, 'r')
-    number = 1
     filename = ''
     membersfound = 0
     members = ''
     for line in content:
-        if number == 3:
+        if line.find('title:')!= -1:
             filename = line.replace('title: ', '')
             filename = filename.replace('\n', '')
-        elif line[0] == 'm':
+        elif line.find('head:') != -1:
+             members = members + line.replace('head: ', '')
+        elif line.find('members:') != -1:
             membersfound = 1
         elif membersfound == 1 and line.find('  -')!= -1:
             members = members + line
         elif membersfound == 1 and line.find('  -')== -1:
             membersfound = 0
-        number = number+1
     name = sFile.replace('.md', '')
     members = members.replace("\n", '')
     members = members.replace("  -", '')
-    file.write(name +" | " + filename  +" | " + members +"\n")
+    file.write(name +" , " + filename  +" , " + members +"\n")
 
 file.close()
 content.close()
+
+
